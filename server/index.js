c5656726adca6db94145a6f277ac4a320d3a14c5
MariusTN <pulsemusic8@gmail.com>
2020-11-27 12:20:09 +0000

    commit

--- server/index.js
+++ server/index.js
@@ -2,19 +2,40 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const yup = require('yup');
 
 const app = express();
 
 app.use(helmet());
-app.use(morgan('tiny'));
-app.use(cors());
+app.use(morgan('combined'));
 app.use(express.json());
+app.use(express.static('./public'));
 
 app.get('/', (req,res) =>{
     res.json({
         message: 'larallyn.link - short urls for your code'
     });
 });
+
+// app.get('/url/:id', (req,res) =>{
+//     //TODO: get a short url by id
+// });
+
+// app.get('/:id', (req,res) =>{
+//     //TODO: redirect to URL
+// });
+
+const schema = yup.object().shape({
+    alias: yup.string().trim().matches(/^[\w\-\d]+$/),
+    url: yup.string().trim().url().required(),
+
+})
+
+app.post('/url', (req,res) =>{
+    //TODO: create a short url
+});
+
+
 
 const port = process.send.PORT || 3000;
 
